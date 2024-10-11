b5ea0db54b72c308e56ee722f0f42691564046ea
AkshayMobileApplicationEngineer <akshaykumarprajapati@hotmail.com>
2024-10-11 23:26:34 +0530

    fix video model

--- src/model/subscription.model.js
+++ src/model/subscription.model.js
@@ -1,21 +1,22 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema } from 'mongoose';
 
 // Define the subscription schema with some typical fields
 const subscriptionSchema = new Schema({
-      subscriber:{
-            type: Schema.Types.ObjectId,//one who is subscribing
-            ref:"Users"
-      },
-      channel:{
-            type:Schema.Types.ObjectId,//one to whom subscriber is subscripting 
-            ref:"User"
-
-      }
-},{
-      timestamps:true
-})
+  subscriber: {
+    type: Schema.Types.ObjectId, // one who is subscribing
+    ref: 'User', // Refers to the User model
+    required: true
+  },
+  channel: {
+    type: Schema.Types.ObjectId, // one to whom the subscriber is subscribing
+    ref: 'User', // Refers to the User model
+    required: true
+  }
+}, {
+  timestamps: true
+});
 
 // Create the Subscription model
-const Subscription = mongoose.model("Subscription", subscriptionSchema);
+const Subscription = mongoose.model('Subscription', subscriptionSchema);
 
 export default Subscription;
